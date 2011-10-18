24bfa4529918a3c92042b7e703cd350ac36a596a
Eric <eric@woodtx.com>
2011-10-18 15:12:03 -0500

    A wild Rails project appears!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FootTraffic::Application.config.secret_token = '719042809138a0eadaaee7d760147cb86ffa257420160b676c033f233d1bd6fdc11181adaaca52c5a003b92eb553fc2cc389af7d98fe7bcb974145245ed11a59'

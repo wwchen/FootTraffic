b57a57eebbede5f72e279ed328ba5544a213d05d
Eric Wood <eric@woodtx.com>
2011-11-15 14:45:09 -0600

    Added traffic pattern calculator.

--- app/models/location.rb
+++ app/models/location.rb
@@ -33,6 +33,43 @@ class Location < ActiveRecord::Base
     end
   end
 
+  # Given a batch of (unique) twitter location IDs, calculates the traffic patterns
+  # for each one and kicks off the Location creation process.
+  def self.process_checkins(loc_ids)
+    # Hash with location ID as key and list of DateTimes as values
+    h = loc_ids.map { |i| [i, Checkin.where(:place_id => i).map { |j| j.created }] }
+    temporal = Hash[h]
+    p temporal
+    
+    daily    = []
+    weekly   = []
+    annually = []
+
+    # Generate the traffic patterns
+    temporal.each do |id,times|
+      times.each do |time|
+        p time
+        daily    << (0..23).to_a.map   { |i| if(time.hour == i) then 1 else 0 end }
+        weekly   << (0..6).to_a.map    { |i| if(time.wday == i) then 1 else 0 end }
+        annually << (0..365).to_a.map  { |i| if(time.yday == i) then 1 else 0 end }
+      end
+    end
+
+    daily_pat  = self.matrix_average(daily)
+    weekly_pat = self.matrix_average(weekly)
+    annual_pat = self.matrix_average(annually)
+  end
+
+  # Given an array of arrays, return an array with the average
+  def self.matrix_average(matrices)
+    matrix = matrices.reduce(Array.new(matrices.first.size).fill(0)) do |m,i|
+      i.zip(m).map { |j| j.sum }
+    end
+    
+    matrix = matrix.map { |i| i / matrix.sum.to_f }
+    return matrix
+  end
+
   # Update all patterns; requires a hash of the form:
   # { :daily => [], :weekly => [], :annually => [] }
   def update_patterns(patterns)

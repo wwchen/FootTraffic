062eff01b330166dfcbdf535497f6fbbacfab88a
William Chen <williamchen16@gmail.com>
2011-11-26 17:26:13 -0600

    Adding a simple search controller to Locations
    - Followed http://railscasts.com/episodes/278-search-with-sunspot

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,12 @@
 class LocationsController < ApplicationController
+  def index
+    @search = Location.search do
+      fulltext params[:search]
+    end
+    @locations = @search.results
+    respond_to do |format|
+      format.html
+      format.json { render :json => @locations }
+    end
+  end
 end

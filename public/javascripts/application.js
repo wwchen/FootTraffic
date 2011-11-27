9504fa0b604077736a8858107f0eb4af9fb08779
Eric Wood <eric@woodtx.com>
2011-11-26 20:33:47 -0600

    Initial version of ajax-ifying the search page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,33 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var search_url = '/locations?q=';
+
+function search()
+{
+  var data = $('#q').val();
+
+  var url = search_url + encodeURI(data);
+  
+  // Clear the results div
+  $('#results').empty();
+
+  $.getJSON(url, function (data) {
+    $.each(data, function(key, value) {
+      var result = $( document.createElement('div') );
+      result.attr('class', 'result');
+
+      var name = $( document.createElement('h3') );
+      name.text(value.location.name);
+      name.appendTo(result);
+
+      var address = $( document.createElement('span') );
+      address.text(value.location.address);
+      address.appendTo(result);
+
+      result.appendTo('#results');
+    });
+  });
+}
+
+$(document).ready(function () {
+  $('#submit').click(function () { search(); });
+});
+
